a91fd7f8c6bdbad8b8c44fb775747dc22763818d
ABDULLAH-SALEEM <79781595+ABDULLAH-SALEEM@users.noreply.github.com>
2021-06-28 23:47:18 +0500

    editing

--- assignments/Assignment7/hotel.js
+++ assignments/Assignment7/hotel.js
@@ -1,5 +1,5 @@
-// const toggleButton=document.getElementsByClassName('toggle-button')[0];
-// const components=document.getElementsByClassName('components')[0];
-// toggleButton.addEventListener('click',()=>{
-//     components.classList.toggle('active')
-// })
+const toggleButton=document.getElementsByClassName('toggle-button')[0];
+const components=document.getElementsByClassName('components')[0];
+toggleButton.addEventListener('click',()=>{
+    components.classList.toggle('active')
+})
