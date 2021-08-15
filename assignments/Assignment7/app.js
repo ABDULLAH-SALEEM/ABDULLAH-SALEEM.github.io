2348e27a24d338bf90c46aade54e355f9a0957d7
ABDULLAH-SALEEM <79781595+ABDULLAH-SALEEM@users.noreply.github.com>
2021-08-16 01:03:39 +0500

    addition

--- assignments/Assignment7/app.js
+++ assignments/Assignment7/app.js
@@ -49,11 +49,38 @@ let rooms = {
     }
 
 };
+
 function checkForm() {
-    var category = document.getElementById('category');
-    var catVal = category.options[category.selectedIndex].value;
-    var className = document.getElementById('class');
-    var classVal = className.options[className.selectedIndex].value;
-    console.log(rooms[catVal][classVal]);
+    const filterDiv=document.getElementsByClassName('filterResult')[0];
+    const myTable=document.getElementById('myTable');
+    const error = document.getElementById('error');
+    error.style.color = 'red';
+    const category = document.getElementById('category');
+    const catVal = category.options[category.selectedIndex].value;
+    const className = document.getElementById('class');
+    const classVal = className.options[className.selectedIndex].value;
+    if (catVal == 'cat' && classVal=='cla') {
+        error.innerHTML = 'Class And Category are Required!';
+        return false;
+    }else if (catVal == 'cat'){
+        error.innerHTML='';
+        error.innerHTML = 'Category is Required!';
+        category.focus();
+        return false;
+    }else if (classVal=='cla'){
+        error.innerHTML='';
+        error.innerHTML = 'Class is Required!';
+        className.focus();
+        return false;}
+    error.innerHTML='';
+    var tableRow=`<tr>
+                   <td>${rooms[catVal][classVal].Room_Type}</td>
+                   <td>${rooms[catVal][classVal].Room_Class}</td>
+                   <td>${rooms[catVal][classVal].Avalaibility}</td>
+                   <td>${rooms[catVal][classVal].Capicity}</td>
+                   <td>${rooms[catVal][classVal].Price}</td>
+    </tr>`
+    filterDiv.style.display='block';
+    myTable.innerHTML=tableRow;
     return false;
 }
