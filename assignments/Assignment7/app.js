3a44edbbcd9a68b756a3f50ae368e7869136d31b
ABDULLAH-SALEEM <79781595+ABDULLAH-SALEEM@users.noreply.github.com>
2021-08-16 21:11:59 +0500

    add

--- assignments/Assignment7/app.js
+++ assignments/Assignment7/app.js
@@ -1,14 +1,14 @@
 let rooms = {
     single_room: {
         First_Class: {
-            Room_Type: 'Single',
+            Room_Type: 'Single Bed',
             Room_Class: 'First Class',
             Avalaibility: 10,
             Capicity: '1 adult',
             Price: 1000,
         },
         Business_Class: {
-            Room_Type: 'Single',
+            Room_Type: 'Single Bed',
             Room_Class: 'Business Class',
             Avalaibility: 6,
             Capicity: '1 adult',
@@ -17,14 +17,14 @@ let rooms = {
     },
     double_room: {
         First_Class: {
-            Room_Type: 'Double',
+            Room_Type: 'Double Bed',
             Room_Class: 'First Class',
             Avalaibility: 10,
             Capicity: '2 adult, 1 Kid',
             Price: 1500,
         },
         Business_Class: {
-            Room_Type: 'Double',
+            Room_Type: 'Double Bed',
             Room_Class: 'Business Class',
             Avalaibility: 6,
             Capicity: '2 adult, 1 Kid',
@@ -49,43 +49,47 @@ let rooms = {
     }
 
 };
-
+const roomClass = document.getElementById('roomClass');
+const roomType = document.getElementById('roomType');
+const bn = document.getElementById('bn');
+const formCover = document.getElementById('formcover');
 function checkForm() {
-    const filterDiv=document.getElementsByClassName('filterResult')[0];
-    const myTable=document.getElementById('myTable');
-    const error = document.getElementById('error');
-    error.style.color = 'red';
     const category = document.getElementById('category');
     const catVal = category.options[category.selectedIndex].value;
     const className = document.getElementById('class');
     const classVal = className.options[className.selectedIndex].value;
-    if (catVal == 'cat' && classVal=='cla') {
+    const filterDiv = document.getElementsByClassName('filterResult')[0];
+    const myTable = document.getElementById('myTable');
+    const error = document.getElementById('error');
+    error.style.color = 'red';
+    if (catVal == 'cat' && classVal == 'cla') {
         error.innerHTML = 'Class And Category are Required!';
         return false;
-    }else if (catVal == 'cat'){
-        error.innerHTML='';
+    } else if (catVal == 'cat') {
+        error.innerHTML = '';
         error.innerHTML = 'Category is Required!';
         category.focus();
         return false;
-    }else if (classVal=='cla'){
-        error.innerHTML='';
+    } else if (classVal == 'cla') {
+        error.innerHTML = '';
         error.innerHTML = 'Class is Required!';
         className.focus();
-        return false;}
-    error.innerHTML='';
-    var tableRow=`<tr>
+        return false;
+    }
+    error.innerHTML = '';
+    var tableRow = `<tr>
                    <td>${rooms[catVal][classVal].Room_Type}</td>
                    <td>${rooms[catVal][classVal].Room_Class}</td>
                    <td>${rooms[catVal][classVal].Avalaibility}</td>
                    <td>${rooms[catVal][classVal].Capicity}</td>
                    <td>${rooms[catVal][classVal].Price}</td>
     </tr>`
-    filterDiv.style.display='block';
-    myTable.innerHTML=tableRow;
+    filterDiv.style.display = 'block';
+    myTable.innerHTML = tableRow;
+    roomClass.value = rooms[catVal][classVal].Room_Class;
+    roomType.value = rooms[catVal][classVal].Room_Type;
     return false;
 }
-const bn=document.getElementById('bn');
-const formCover=document.getElementById('formcover');
-function onClick(){
-    formCover.style.display='block';
+function onClick() {
+    formCover.style.display = 'block';
 }
